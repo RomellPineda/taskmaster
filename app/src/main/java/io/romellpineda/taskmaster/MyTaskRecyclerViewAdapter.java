f9da5f7c328148080c1fe0fd765bf3df9200d57d
Romell Pineda <romell.pineda@gmail.com>
2020-02-13 11:13:47 -0800

    view adapter and fragment

---
+++ app/src/main/java/io/romellpineda/taskmaster/MyTaskRecyclerViewAdapter.java
@@ -0,0 +1,85 @@
+package io.romellpineda.taskmaster;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import io.romellpineda.taskmaster.TaskFragment.OnListFragmentInteractionListener;
+import io.romellpineda.taskmaster.dummy.DummyContent.DummyItem;
+
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyTaskRecyclerViewAdapter extends RecyclerView.Adapter<MyTaskRecyclerViewAdapter.ViewHolder> {
+
+    static final String TAG = "////";
+    private final List<Task> mValues;
+    private final OnListFragmentInteractionListener mListener;
+
+    public MyTaskRecyclerViewAdapter(List<Task> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_task, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.myTitleView.setText(mValues.get(position).getTitle());
+        holder.mBodyView.setText(mValues.get(position).getBody());
+        holder.mStateView.setText(mValues.get(position).getState());
+
+        holder.myTitleView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "thingy clicked");
+//                if (null != mListener) {
+//                     Notify the active callbacks interface (the activity, if the
+//                     fragment is attached to one) that an item has been selected.
+//                    mListener.onListFragmentInteraction(holder.mItem);
+//                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView myTitleView;
+        public final TextView mBodyView;
+        public final TextView mStateView;
+        public Task mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            myTitleView = (TextView) view.findViewById(R.id.title);
+            mBodyView = (TextView) view.findViewById(R.id.body);
+            mStateView = (TextView) view.findViewById(R.id.state);
+
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mBodyView.getText() + "'";
+        }
+    }
+}
