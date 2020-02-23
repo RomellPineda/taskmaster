2d6aeb53b308520268863ba242e97dd4525703b0
Romell Pineda <romell.pineda@gmail.com>
2020-02-22 21:07:30 -0800

    first tasks

--- app/src/main/java/io/romellpineda/taskmaster/AddActivity.java
+++ app/src/main/java/io/romellpineda/taskmaster/AddActivity.java
@@ -1,6 +1,7 @@
 package io.romellpineda.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +10,22 @@ import android.widget.Toast;
 
 public class AddActivity extends AppCompatActivity {
 
+    TaskDatabase taskDb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        taskDb = Room.databaseBuilder(getApplicationContext(), TaskDatabase.class, "task").allowMainThreadQueries().build();
+
+        Task run = new Task("run", "do some cardio", "incomplete");
+        Task clean = new Task("clean", "as needed", "incomplete");
+        Task sleep = new Task("sleep", "required", "incomplete");
+        taskDb.taskDao().saveTask(run);
+        taskDb.taskDao().saveTask(clean);
+        taskDb.taskDao().saveTask(sleep);
+
 
         Button publishTask = findViewById(R.id.button3);
         publishTask.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/io/romellpineda/taskmaster/TaskDatabase.java
@@ -0,0 +1,9 @@
+package io.romellpineda.taskmaster;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Task.class}, version = 1)
+public abstract class TaskDatabase extends RoomDatabase {
+    public abstract TaskDao taskDao();
+}
